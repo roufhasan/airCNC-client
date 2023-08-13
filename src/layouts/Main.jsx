c7ed733dd1f6cab5ae920128005261da71e81a59
roufhasane <roufhasan5@gmail.comm>
2023-08-13 22:00:57 +0600

    first commit

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Shared/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="pt-28 pb-20">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/Signup/Signup";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+]);
